9e2452e4c8fab441370723f83d4ed1a211d70de2
Sumanben Kishorbhai Sewani <sksewani@mtrx-node01pd.dcm.senecacollege.ca>
2021-02-01 23:18:49 -0500

    lab3

---
+++ lab3d.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess 
+
+P = subprocess.Popen(["df -h | grep '/$' | awk '{print $4}'"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+P_contents = P.communicate()
+
+#result = P.read()
+#P.close()
+
+def free_space():
+	output = P_contents[0].decode('utf-8').strip()
+	return output
+if __name__=='__main__':
+	print(free_space())
+
+
+
+
+
